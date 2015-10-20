78b0c527de7629c08859f07586a6466896f64463
Brian Fountain <brian@brianfountain.com>
2015-10-20 14:50:37 -0400

    Add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def current_user
-  	session[:user_id] ? User.find(session[:user_id]) : nil
-  end
-
-  def authenticate_user!
-  	unless current_user
-  		redirect_to login_path, notice: "Please log in first."
-  	end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:username, :location, :bio]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  get '/login', to: 'sessions#new'
-  post '/sessions', to: 'sessions#create'
-  delete '/sessions', to: 'sessions#destroy'
+  devise_for :users
+  # get '/login', to: 'sessions#new'
+  # post '/sessions', to: 'sessions#create'
+  # delete '/sessions', to: 'sessions#destroy'
 
   resources :users do
     member do
